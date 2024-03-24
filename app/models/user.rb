d94a402c4061aa22388431958a97bd5107ebdbf1
Bara <bkonateh1@gmail.com>
2024-03-24 19:34:37 +0100

    Update user.rb and _navbar.html.haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, confirmation: true
-  validates :user_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+  validates :password, presence: true, confirmation: true, length: { minimum: 6, message: "must be at least 6 characters"}
+  validates :user_name, presence: true, uniqueness: true, length: { minimum: 3, message: "must be at least 3 characters"}
 
 end
