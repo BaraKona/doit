a7a944d7b9bca129b48a4d0a7e0c11f4aa6144c9
Bara <bkonateh1@gmail.com>
2024-03-24 19:59:58 +0100

    Add form for signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "todos" => "todos#index"
+
+  get "signup" => "signup#new"
+  post "signup" => "signup#create"
+
+
   root "home#index"
   # Defines the root path route ("/")
   # root "posts#index"
