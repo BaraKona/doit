81bf554a732d48e46154d6ad8eb06bbf7ee4a2b7
Bara <bkonateh1@gmail.com>
2024-03-25 17:25:54 +0100

    Update redirect path and label styles in signup controller and view

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -8,7 +8,7 @@ class SignupController < ApplicationController
 
     if @user.save # Save the user to the database
       session[:user_id] = @user.id # Log in the user
-      redirect_to todos_path # Redirect to the todos page
+      redirect_to todos_path(view: 'all') # Redirect to the todos page
     else
       flash[:alert] = @user.errors.full_messages.join(", ") # Set the flash alert message
       render :new, status: :unprocessable_entity # Render the new view
