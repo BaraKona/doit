3081d2d22a6ab0600dc23faa07c1e096d444dd0d
Bara <bkonateh1@gmail.com>
2024-03-25 09:33:25 +0100

    refactor user to be stored as current attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :set_current_user
 
+  private
+
   def set_current_user
-    if session[:user_id]
-      @current_user = User.find_by(id: session[:user_id])
-    end
+    Current.user = User.find_by(id: session[:user_id])
   end
 
 
